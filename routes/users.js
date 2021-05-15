36c6e6379e57ebdcad8ee1ad4b3fd949caae6955
sejaljain123 <sejaljain2043@gmail.com>
2021-05-15 17:24:26 +0530

    renamed files

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var register_controller = require('../controllers/registercontroller');
-var signin_controller = require('../controllers/signincontroller');
+var register_controller = require('../controllers/register');
+var signin_controller = require('../controllers/signin');
 router.post('/register', register_controller.handleRegister);
 router.post('/signin', signin_controller.handleSignin);
 module.exports = router;
