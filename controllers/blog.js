0b55c6d6e45e27ef60340e82d520cf13e2920311
sejaljain123 <sejaljain2043@gmail.com>
2021-06-07 14:37:01 +0530

    Update Blog Api

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,9 @@
+const { TokenExpiredError } = require('jsonwebtoken');
 const Blog = require('../models/blog');
-const User = require('../models/user');
-
+const display_blog = async (req, res) => {
+  const posts = await Blog.find({}).populate('created_by');
+  res.json({ posts, message: 'display posts' });
+};
 const create_blog = async (req, res) => {
   const blog = new Blog({
     title: req.body.title,
@@ -9,7 +12,7 @@ const create_blog = async (req, res) => {
     created_at: req.body.created_at,
     created_by: req.decodedToken.userId,
   });
-  console.log(req.body);
+
   await blog
     .save()
     .then((blog) => {
@@ -30,37 +33,50 @@ const create_blog = async (req, res) => {
     });
 };
 
-const display_blog = async (req, res) => {
-  const posts = await Blog.find({}).populate('created_by');
-  res.json({ posts, message: 'display posts' });
-};
-const display_userblog = async (req, res) => {
+const read_blog = async (req, res) => {
   const posts = await Blog.findById({ _id: req.params.id }).populate('created_by');
   res.status(200).json({
     message: 'Posts fetched successfully',
     posts,
   });
 };
+
+const user_blog = async (req, res) => {
+  const posts = await Blog.find({ created_by: req.decodedToken.userId }).populate('created_by');
+
+  res.json({ posts, message: 'Posts' });
+
+  // if (req.decodedToken.userId === String(post.created_by)) {
+  // }
+};
+
+const update_blog = async (req, res) => {
+  const post = await Blog.findById({ _id: req.params.id });
+  if (req.decodedToken.userId === String(post.created_by)) {
+    post.title = req.body.title;
+    post.description = req.body.description;
+    post.content = req.body.content;
+
+    await post.save();
+    res.json({ message: 'Updated' });
+  } else {
+    res.json({ message: 'unauthorized' });
+  }
+};
 const delete_post = async (req, res) => {
-  await Blog.findOneAndDelete({ _id: req.params.id }, function (err, post) {
-    if (err) {
-      return next(err);
-    }
-    if (req.decodedToken.userId === user._id) {
-      if (!post) {
-        return res.json({ message: 'Post does not exist!' });
-      }
-      return res.json({ message: 'post deleted' });
-    } else {
-      return res.json({ message: 'Not authorized' });
-    }
-  });
+  const post = await Blog.findById({ _id: req.params.id });
+  console.log(post);
+  if (req.decodedToken.userId === String(post.created_by)) {
+    await Blog.deleteOne(post);
+    res.json({ success: true, message: 'Post Deleted' });
+  } else res.json({ success: false, message: 'Unauthorized' });
 };
 
 module.exports = {
   create_blog,
   display_blog,
-
+  update_blog,
   delete_post,
-  display_userblog,
+  read_blog,
+  user_blog,
 };
--- routes/blog.js
+++ routes/blog.js
@@ -4,7 +4,9 @@ const auth = require('../middleware/auth');
 var Blog = require('../controllers/blog');
 router.post('/create', auth, Blog.create_blog);
 router.get('/display', auth, Blog.display_blog);
-router.get('/myblog/:id', auth, Blog.display_userblog);
+router.get('/myblog/:id', auth, Blog.read_blog);
 router.delete('/delete/:id', auth, Blog.delete_post);
+router.put('/update/:id', auth, Blog.update_blog);
+router.get('/myblogs', auth, Blog.user_blog);
 
 module.exports = router;
