b96bc224dfa37551b17809579a8bdde214d44c9b
sejaljain123 <sejaljain2043@gmail.com>
2021-06-09 14:00:45 +0530

    added app.get

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const start = async () => {
   app.use(express.json());
   app.use('/', userRouter);
   app.use('/blog', blogRouter);
-
+  app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
   app.listen(process.env.PORT || 5000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
   });
